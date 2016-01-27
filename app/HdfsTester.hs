1f93a24868ae276110a3630994eaaeb7924b38d3
Axel Mannhardt <axel.mannhardt@freiheit.com>
2016-01-27 17:30:29 +0100

    extended tester with copyToLocal

--- app/HdfsTester.hs
+++ app/HdfsTester.hs
@@ -1,6 +1,7 @@
 import System.Environment (getArgs)
 
 import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.IO as TL
 import System.HDFS.HDFSClient
 
 main :: IO ()
@@ -10,4 +11,5 @@ main = do
    ["ls", host, port, path] -> hdfsListFiles (host, read port) path >>= print
    ["blockLocations", host, port, path] -> hdfsFileDistribution (host, read port) path >>= print
    ["readlen", host, port, path] -> hdfsReadCompleteFile (host, read port) path >>= return . TL.length >>= print
-   _ -> error "usage: <ls|blockLocations|readlen> <host> <port> <path>"
+   ["copyToLocal", localPath, host, port, path] -> hdfsReadCompleteFile (host, read port) path >>= TL.writeFile localPath
+   _ -> error "usage: <ls|blockLocations|readlen|copyToLocal <localPath>> <host> <port> <path>"
